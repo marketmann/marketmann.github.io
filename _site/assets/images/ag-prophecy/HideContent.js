0a8fb171c64bc26084c129a200d9c29db227a7e5
theday1smine <57020343+theday1smine@users.noreply.github.com>
2020-05-03 13:51:57 -0700

    sd

---
+++ _site/assets/images/ag-prophecy/HideContent.js
@@ -0,0 +1,26 @@
+﻿function __showElements(elements){
+    for (var index in elements){
+        elements[index].style.display = "inline-block";
+    }
+}
+  
+var __subscribeNowElements = [];
+  
+function __showSubscribeNow(){
+    __subscribeNowElements = __getByClassName('subscribeNow');
+    if (__getCookie(document.location.href)) {
+        __showElements(__subscribeNowElements);
+    }else{
+        setTimeout(function () {
+            __showElements(__subscribeNowElements);
+        }, __subscribeNowDelay);
+        __setCookie(document.location.href, "visited",365);
+    }
+}
+  
+if (self !== top) {
+    setTimeout(__showSubscribeNow, 200);
+} else {
+    __attachEventHandler(window, 'load', __showSubscribeNow);
+}
+
